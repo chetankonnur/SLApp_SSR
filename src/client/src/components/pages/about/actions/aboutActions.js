3df37394aecff07548a15abbe89d00692a9a3a96
chetan konnur <chetan.konnur@yasco.in>
2019-12-10 18:37:13 +0530

    updated action server path

--- src/client/src/components/pages/about/actions/aboutActions.js
+++ src/client/src/components/pages/about/actions/aboutActions.js
@@ -1,12 +1,10 @@
-import { HTTPRequestHandler } from "../../../../utils/commonRequires";
+import { HTTPRequestHandler } from "../../../utils/commonRequires";
 
 export const fetchCampaigns = params => {
   return new Promise((resolve, reject) => {
-    const fetchCampaignsUrl = "http://localhost:5001/campaigns";
+    const fetchCampaignsUrl = "https://sazendev.sevagarden.org/api/campaigns";
     HTTPRequestHandler.get(fetchCampaignsUrl)
       .then(response => {
-        console.log('act',response);
-        
         resolve({
           about: {
             posts: response.data.campaigns
