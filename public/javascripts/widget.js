d5811afaa40ae11c48d5d287def70c0fc8b931ad
chetan konnur <chetan.konnur@yasco.in>
2019-12-11 17:01:39 +0530

    added ajax script file

---
+++ public/javascripts/widget.js
@@ -0,0 +1,15 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>;
+var settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://afternoon-coast-12049.herokuapp.com/about",
+  method: "GET",
+  headers: {
+    Accept: "*/*",
+    Host: "afternoon-coast-12049.herokuapp.com"
+  }
+};
+$.ajax(settings).done(function(response) {
+  $("#replaceDiv").html(response);
+});
+<div id="replaceDiv"></div>;
