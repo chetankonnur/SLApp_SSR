adee2326f5344133eec79e0d2c7ddbbc60e2c56e
chetan konnur <chetan.konnur@yasco.in>
2019-12-16 18:41:04 +0530

    updated 1

--- public/javascripts/widget.js
+++ public/javascripts/widget.js
@@ -1,4 +1,5 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>;
+
+{/* <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>; */}
 
 var settings = {
   async: true,
@@ -15,9 +16,9 @@ var settings = {
 const fun1 = (param) => {
   console.log('param---------------------------------> ',param);
   
-  $.ajax(settings).done(function(response) {
-    $("#replaceDiv").html(response);
-  });
+  // $.ajax(settings).done(function(response) {
+  //   $("#replaceDiv").html(response);
+  // });
 }
 
 <div id="replaceDiv"></div>;
